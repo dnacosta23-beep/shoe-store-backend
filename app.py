9d6c2cd779570db2e444f666fbcc6d056b7aed12
Danielle Acosta <dnacosta23@gmail.com>
2026-06-24 17:49:59 -0500

    Home route, Shoe (GET) and add a new shoe (POST) created. Error handling created.

--- app.py
+++ app.py
@@ -11,3 +11,33 @@ shoes = [
 ]
 
 
+@app.route("/")
+def home():
+    return {"message": "Shoe Store API"}
+
+
+@app.route("/shoes", methods=["GET"])
+def get_shoes():
+    return shoes
+
+
+@app.route("/shoes", methods=["POST"])
+def add_shoe():
+    data = request.get_json()
+
+    new_shoe = {
+        "id": len(shoes) + 1,
+        "name": data["name"],
+        "price": data["price"]
+    }
+
+    shoes.append(new_shoe)
+
+    return {
+        "message": "Shoe added successfully!",
+        "shoe": new_shoe
+    }, 201
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
